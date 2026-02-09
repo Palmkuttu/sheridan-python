afe47bf7fd97af3d35239d2dae60b3460fd6b157
Dennis Zacharia <dennyzach15@gmail.com>
2026-02-09 08:33:49 -0500

    Remove tests for generate_prime_factors
    Removed unit tests for generate_prime_factors function.

--- prime.py
+++ prime.py
@@ -15,30 +15,3 @@ def generate_prime_factors(number: int) -> list[int]:
         divisor += 1
     return factors
 
-def test_input_non_integer():
-    with pytest.raises(ValueError):
-        generate_prime_factors("string")
-
-    with pytest.raises(ValueError):
-        generate_prime_factors(3.14)
-
-def test_input_1_returns_empty():
-    assert generate_prime_factors(1) == []
-
-def test_input_2_returns_2():
-    assert generate_prime_factors(2) == [2]
-
-def test_input_3_returns_3():
-    assert generate_prime_factors(3) == [3]
-
-def test_input_4_returns_2_2():
-    assert generate_prime_factors(4) == [2, 2]
-
-def test_input_5_returns_2_3():
-    assert generate_prime_factors(6) == [2, 3]
-
-def test_input_6_returns_2_2_2():
-    assert generate_prime_factors(8) == [2, 2, 2]
-
-def test_input_7_returns_3_3():
-    assert generate_prime_factors(9) == [3, 3]
